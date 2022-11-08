0841e68ade53c4d34039f520690b158c85085f34
Amit Datta <ak.datta2648@gamil.com>
2022-11-09 01:08:11 +0600

    Login page design complete

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,45 +1,71 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import lottie from 'lottie-web';
+import { FaFacebook } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
+
+    const container = useRef(null);
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: container.current,
+            renderer: 'svg',
+            autoplay: true,
+            loop: true,
+            animationData: require('../../../LottieFiles/login.json'),
+        })
+    }, []);
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+    }
+
     return (
-        <div className="hero my-4">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero">
+            <div className="hero-content flex-col-reverse lg:flex-row-reverse lg:gap-12 ">
 
-
-
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                </div>
-
-
-
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <h2 className='text-center text-3xl font-bold mb-6'>Please Login</h2>
+                        <div className="form-control mb-3">
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
-                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button type='submit' className="btn btn-primary">Login</button>
                         </div>
-                    </div>
+                        <div>
+                            <p className='text-[14px]'>Don't Have an Account? <Link className='text-primary hover:underline' to='/register'>Register Now</Link></p>
+                        </div>
+                        <div className='flex items-center my-4'>
+                            <div className='h-[1px] w-20 md:w-32 bg-[#ccc]'></div>
+                            <p className='text-center'>Login With</p>
+                            <div className='h-[1px] w-20 md:w-32 bg-[#ccc]'></div>
+                        </div>
+                        <div className='flex justify-center gap-4'>
+                            <button>
+                                <FcGoogle className='text-[36px]' />
+                            </button>
+                            <button>
+                                <FaFacebook className='text-[#4292FF] text-[32px]' />
+                            </button>
+                        </div>
+                    </form>
+
                 </div>
 
+                <div className="text-center lg:text-left">
+                    <div className='w-full max-w-2xl' ref={container}></div>
+                </div>
 
-                
             </div>
         </div>
     );
